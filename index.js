f948ddb3943b81e1419bdcc31777152b45e766d0
NurtyJhons <95722271+NurtyJhons@users.noreply.github.com>
2023-10-11 14:49:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function imprimirOperacoes(a, b) {
+  console.log(`${a} + ${b} = ${a + b}`)
+  console.log(`${a} - ${b} = ${a - b}`)
+  console.log(`${a} x ${b} = ${a * b}`)
+  console.log(`${a} / ${b} = ${a / b}`)
+}
